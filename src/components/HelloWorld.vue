779e0df74a8147bbe8d98e3a4bad00809fb6b3b2
nlukic97 <nlukic97@gmail.com>
2020-06-06 13:55:25 +0200

    link to imbd created in a tag

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,15 @@
   <div class="hello">
     <input v-model="searchTerm" type="text" v-on:keyup="updateList">
     <ul v-if="list">
+
       <li v-for="(item, index) in list" v-bind:key="index">
-        <img v-bind:src="item.Poster" alt="" v-on:click="openInImdb(item.imdbID)" title="Open in imbd">
-        <h2 v-on:click="openInImdb(item.imdbID)" title="Open in imbd">{{item.Title}} ({{item.Year}})</h2>
-        <p v-on:click="openInImdb(item.imdbID)" title="Open in imbd">{{item.Type}}</p>
+        <a v-bind:href="item.imbdID" target="_target" title="Open in imbd">
+          <img v-bind:src="item.Poster" alt="">
+          <h2>{{item.Title}} ({{item.Year}})</h2>
+          <p>{{item.Type}}</p>
+        </a>
       </li>
+
     </ul>
     <p v-if="error">{{this.error}}</p>
   </div>
@@ -32,8 +36,15 @@ export default {
           if(response.data.Response == 'True'){
             this.list = response.data.Search;
             this.error = null;
-            // console.log(response.data.Search[0]['imdbID'])
-            console.log(response.data.Search)
+            console.log(this.list)
+            
+            for(var i = 0; i < this.list.length; i++){  // Zasto ovde ispod vucem ovako ?
+              this.list[i].imbdID =' https://www.imdb.com/title/' + this.list[i]['imdbID'] + '/'
+              // this.list[i].imbdID =' https://www.imdb.com/title/' + this.list[i].imdbID + '/' // zasto ne bi ovo radilo ?
+              
+              // console.log(this.list[i].imbdID)
+              // console.log(this.list[i]['imbdID'])
+            }
           } else {
             if(response.data.Error == 'Something went wrong.'){
               this.error = 'Search for something...';
@@ -46,7 +57,8 @@ export default {
         .catch(this.error = 'Something went wrong. Please try again')
     },
     openInImdb: function(id){
-      // console.log(id);
+      console.log(id);
+      // console.log(this.list)
       window.open('https://www.imdb.com/title/'+ id + '/');
     }
   }
@@ -63,7 +75,10 @@ export default {
     list-style-type: none;
     li{
       margin-bottom:35px;
-      img{
+      a {
+        text-decoration: none;
+        color:#333;
+        img{
         &:hover {
         cursor:pointer;
         }
@@ -80,6 +95,7 @@ export default {
         &:hover {
         cursor:pointer;
         }
+      }
       }
     }
   }
